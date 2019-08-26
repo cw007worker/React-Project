448337cdf0e0501a5ab49903e065929cdf6efc44
linwinpei <52321057+linwinpei@users.noreply.github.com>
2019-08-26 23:12:07 +0800

    Toolbar added in NavBar by importing material-ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import NavBar from './NavBar';
 
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <NavBar />
+        <p>content</p>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/NavBar.jsx
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+class NavBar extends Component {
+    render() {
+        return (
+            <AppBar>
+                <Toolbar />
+            </AppBar>
+        );
+    }
+}
+
+export default NavBar;
