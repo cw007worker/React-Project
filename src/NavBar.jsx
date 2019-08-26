73f346724e8ca66a0ee1c45f2c67900086b553b3
linwinpei <52321057+linwinpei@users.noreply.github.com>
2019-08-26 23:43:23 +0800

    add params from react-easy-stack for synchronization with searchbar value

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import SearchBar from 'material-ui-search-bar';
+import { view, params } from 'react-easy-stack';
+
+const toolbarStyle = {
+    maxWidth: 800,
+    margin: '0 auto'
+};
 class NavBar extends Component {
+    onSearch = (filter) => {
+        params.search = filter;
+    }
     render() {
         return (
             <AppBar>
-                <Toolbar><SearchBar onChange={() => console.log('onChange')}
-                    onRequestSearch={() => console.log('onRequestSearch')}
+                <Toolbar style={toolbarStyle}><SearchBar
+                    onRequestSearch={this.onSearch}
+
 
                 /></Toolbar>
             </AppBar>
