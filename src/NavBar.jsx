b4317a4556700642022ccf97284a4f1c8b810d9e
linwinpei <52321057+linwinpei@users.noreply.github.com>
2019-08-26 23:22:57 +0800

    add SearchBar component from material-ui-search-bar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,11 +1,15 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
+import SearchBar from 'material-ui-search-bar';
 class NavBar extends Component {
     render() {
         return (
             <AppBar>
-                <Toolbar />
+                <Toolbar><SearchBar onChange={() => console.log('onChange')}
+                    onRequestSearch={() => console.log('onRequestSearch')}
+
+                /></Toolbar>
             </AppBar>
         );
     }
